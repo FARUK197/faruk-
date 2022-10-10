2e2692788c766d2808c892f181f61ddc7ca0f37c
FARUK197 <omarfarukrokon@gmail.com>
2022-10-10 16:06:45 +0600

    first commit

---
+++ assign3.py
@@ -0,0 +1,13 @@
+# sum=0
+# for a in range(0,101,1):
+# sum+=a
+# print("Sum is :",sum)
+
+########---dictionary-----------------
+
+farukdict = {
+  "Name": "Faruk",
+  "Address": "Dhaka",
+  "ID": '0056'
+}
+print(farukdict)
